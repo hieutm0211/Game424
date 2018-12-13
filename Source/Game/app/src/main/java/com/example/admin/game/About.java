59bf7b4494dcd92d629b2161c8f07155a217ca23
hieu tran minh <hieutranminh211@gmail.com>
2018-12-13 21:53:34 +0700

    Update

--- Source/Game/app/src/main/java/com/example/admin/game/About.java
+++ Source/Game/app/src/main/java/com/example/admin/game/About.java
@@ -12,15 +12,15 @@ public class About extends Activity {
         super.onCreate(savedState);
         setContentView(R.layout.about);
         tv_about = (TextView) findViewById(R.id.tv_about);
-        tv_about.setText("Game424 \n " +
+        tv_about.setText("Game424 \n\n " +
                 "This game is written by H2 team of University of Information Technology.\n" +
-                "The minimum system requirements for android 0S: 5.0+"+
-                "Members of team:\n" +
+                "The minimum system requirements for android 0S: 5.0+\n\n\n"+
+                "Members of team:\n\n" +
                 "Hieu Tran Minh - Software Engineer\n" +
-                "-+-+-+-+-+-"+
+                "-+-+-+-+-+-\n"+
                 "Huy Vo Quoc - Software Engineer\n" +
                 "\n\n\n\n\n\n"+
-                "-+-+-+-+-+-"+
+                "-+-+-+-+-+-\n"+
                 "Contact: (+84)976086911\n" +
                 "Email: hieutranminh211@gmail.com\n");
     }
